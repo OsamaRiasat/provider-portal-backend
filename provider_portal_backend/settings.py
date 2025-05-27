57db112f35bfc82a4b7a196f19bfc615eb457c54
Usama Riasat <osamariasat@gmail.com>
2025-05-27 16:28:51 +0500

    deployment 2

--- provider_portal_backend/settings.py
+++ provider_portal_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=7b(bd7v#nv+u^n@cgq0*q7)n(mccqou6&^_=-t1(qmizp-xp+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # Add this at the top
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
